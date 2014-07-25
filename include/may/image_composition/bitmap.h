4abeb029a0909e621052fc67bdb711cfd6a403c9
dogsaur <dogsaur@gmail.com>
2014-07-25 22:59:20 +0800

    完成Bitmap相关写操作
    实现BitmapImage.Save操作，暂时只支持24bit

--- include/may/image_composition/bitmap.h
+++ include/may/image_composition/bitmap.h
@@ -2,107 +2,97 @@
 #define MAY_IMAGE_COMPOSITION_BITMAP
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <vector>
 #include <fstream>
 namespace may
 {
-namespace image_composition
-{
-typedef struct Pix Color;
-struct Pix
-{
-	Pix(){}
-    Pix(short r, short g, short b)
-    {
-        R = r;
-        G = g;
-        B = b;
-    }
-    short R;
-    short G;
-    short B;
-};
-typedef unsigned char UInt8;
-typedef unsigned short UInt16;
-//typedef unsigned int UInt32;
-typedef unsigned long UInt32;
-typedef unsigned long long UInt64;
-typedef char Int8;
-typedef short Int16;
-//typedef int Int32;
-typedef long Int32;
-typedef long long Int64;
+	namespace image_composition
+	{
+		typedef struct Pix Color;
+		typedef unsigned char UInt8;
+		typedef unsigned short UInt16;
+		//typedef unsigned int UInt32;
+		typedef unsigned long UInt32;
+		typedef unsigned long long UInt64;
+		typedef char Int8;
+		typedef short Int16;
+		//typedef int Int32;
+		typedef long Int32;
+		typedef long long Int64;
+		struct Pix
+		{
+			Pix(){}
+			Pix(UInt8 r, UInt8 g, UInt8 b)
+			{
+				R = r;
+				G = g;
+				B = b;
+			}
+			UInt8 R;
+			UInt8 G;
+			UInt8 B;
+		};
 
-struct BmpInfoHeader
-{
-    // size of this header in the file
-    UInt32 info_size;
-    // image dimensions
-    Int32 width, height;
-    // number of planes, always set to one
-    UInt16 planes;
-    // bits per pixel
-    UInt16 bit_count;
-    // compression type
-    UInt32 compression;
-    // image size in bytes, may be zero for 24 bpp images
-    UInt32 image_size;
-    // image resolution
-    Int32 x_pixels_per_meter, y_pixels_per_meter;
-    // number of used colors, may be zero
-    UInt32 clr_used;
-    // number of important colors, may be zero
-    UInt32 clr_important;
-    void from_stream( std::ifstream & stream);
-};
 
-struct BmpFileHeader
-{
-    // attributes
+		struct BmpInfoHeader
+		{
+			UInt32 info_size;			// size of this header in the file
+			Int32 width, height;		// image dimensions
+			UInt16 planes;				// number of planes, always set to one
+			UInt16 bit_count;			// bits per pixel
+			UInt32 compression;			// compression type
+			UInt32 image_size;			// image size in bytes, may be zero for 24 bpp images
+			Int32 x_pixels_per_meter, y_pixels_per_meter; // image resolution
+			UInt32 clr_used;			// number of used colors, may be zero
+			UInt32 clr_important;		// number of important colors, may be zero
+			void from_stream(std::fstream & stream);
+			void to_stream(std::fstream &stream);
+		};
 
-    // the magic number "BM"
-    UInt16 magic;
-    // size of the whole file
-    UInt32 size;
-    // offset (from this field) to the data
-    UInt32 offset;
+		struct BmpFileHeader
+		{
+			UInt16 magic;    // the magic number "BM"
+			UInt32 size;    // size of the whole file
+			UInt32 offset;    // offset (from this field) to the data
 
-    // methods
-    BmpFileHeader()
-    {
-        magic = 0x4D42;
-    }
-    void from_stream( std::ifstream & stream);
-};
+			BmpFileHeader()
+			{
+				magic = 0x4D42;
+			}
+			void from_stream(std::fstream & stream);
+			void to_stream(std::fstream &stream);
+		};
 
-class BitmapImage
-{
-public:
-    std::vector< std::vector<Pix> > Pixels;
-    BitmapImage();
-    BitmapImage(std::string& filename);
-    int GetWidth();
-    int GetHeight();
-    int Load(const std::string& filename);
-private:
-    int width;
-    int height;
-    BmpFileHeader file_header;
-    BmpInfoHeader info_header;
-	bool grayscale;
-    std::ifstream stream;
-    std::vector< std::vector<Color> >color_table;
+		class BitmapImage
+		{
+		public:
+			std::vector< std::vector<Pix> > Pixels;
+			BitmapImage();
+			BitmapImage(std::string& filename);
+			int GetWidth();
+			int GetHeight();
+			int Load(const std::string& filename);
+			int Save(const std::string& filename);
+		private:
+			int width;
+			int height;
+			BmpFileHeader file_header;
+			BmpInfoHeader info_header;
+			bool grayscale;
+			std::fstream stream;
+			std::vector< std::vector<Color> >color_table;
 
-    void read_data ();
-    void read_color_table ();
-    void read_1bit_data ();
-    void read_rle4_data ();
-    void read_4bit_data ();
-    void read_rle8_data ();
-    void read_8bit_data ();
-    void read_rgb_data ();
-};
-}
+			void read_data();
+			void read_color_table();
+			void read_1bit_data();
+			void read_rle4_data();
+			void read_4bit_data();
+			void read_rle8_data();
+			void read_8bit_data();
+			void read_rgb_data();
+
+			void write_data();
+		};
+	}
 }
 #endif
--- src/bitmap.cpp
+++ src/bitmap.cpp
@@ -3,7 +3,7 @@ namespace may
 {
 	namespace image_composition
 	{
-		void BmpFileHeader::from_stream(std::ifstream & stream)
+		void BmpFileHeader::from_stream(std::fstream &stream)
 		{
 			UInt16 filemagic;
 			stream.read((char*)&filemagic, sizeof(filemagic));
@@ -11,8 +11,32 @@ namespace may
 			stream.seekg(4, std::ios::cur);
 			stream.read((char*)&offset, sizeof(offset));
 		}
+		void BmpFileHeader::to_stream(std::fstream &stream)
+		{
+			stream.write((char*)&magic, sizeof(magic));
+			stream.write((char*)&size, sizeof(size));
+			for (unsigned int i = 0; i < 4; ++i)
+				stream.put(0); // clear reserved fields
+			stream.write((char*)&offset, sizeof(offset));
+		}
 
-		void BmpInfoHeader::from_stream(std::ifstream & stream)
+		void BmpInfoHeader::to_stream(std::fstream &stream)
+		{
+			planes = 1;
+			stream.write((char*)&info_size, sizeof(info_size));
+			stream.write((char*)&width, sizeof(width));
+			stream.write((char*)&height, sizeof(height));
+			stream.write((char*)&planes, sizeof(planes));
+			stream.write((char*)&bit_count, sizeof(bit_count));
+			stream.write((char*)&compression, sizeof(compression));
+			stream.write((char*)&image_size, sizeof(image_size));
+			stream.write((char*)&x_pixels_per_meter, sizeof(x_pixels_per_meter));
+			stream.write((char*)&y_pixels_per_meter, sizeof(y_pixels_per_meter));
+			stream.write((char*)&clr_used, sizeof(clr_used));
+			stream.write((char*)&clr_important, sizeof(clr_important));
+		}
+
+		void BmpInfoHeader::from_stream(std::fstream & stream)
 		{
 			const UInt32 info_impl_size = 40;
 			stream.read((char*)&info_size, sizeof(info_size));
@@ -30,7 +54,7 @@ namespace may
 		}
 
 
-		
+
 		BitmapImage::BitmapImage() {}
 		BitmapImage::BitmapImage(std::string& filename)
 		{
@@ -46,7 +70,7 @@ namespace may
 		}
 		int BitmapImage::Load(const std::string& filename)
 		{
-			stream.open(filename.c_str());
+			stream.open(filename.c_str(), std::ios::in);
 			if (!stream.good())
 			{
 				return 1;
@@ -61,7 +85,22 @@ namespace may
 			width = info_header.width;
 			height = info_header.height;
 			read_data();
+			stream.close();
 			return 0;
+		}
+		int BitmapImage::Save(const std::string &filename)
+		{
+			stream.open(filename.c_str(), std::ios::out);
+			if (!stream.good())
+			{
+				return 1;
+			}
+			file_header.to_stream(stream);
+			info_header.to_stream(stream);
+			write_data();
+			stream.close();
+			return 0;
+
 		}
 		void BitmapImage::read_data()
 		{
@@ -121,6 +160,24 @@ namespace may
 				}
 				stream.seekg(pad_size, std::ios::cur);
 			}
+		}
+		void BitmapImage::write_data()
+		{
+			const unsigned int line_size = 3 * info_header.width;
+			const unsigned int pad_size = (line_size % 4) ? 4 - (line_size % 4) : 0;
+			for (int row = info_header.height - 1; row >= 0; --row)
+			{
+				for (int col = 0; col < info_header.width; ++col)
+				{
+
+					stream.put(Pixels[row][col].B);
+					stream.put(Pixels[row][col].G);
+					stream.put(Pixels[row][col].R);
+				}
+				stream.seekg(pad_size, std::ios::cur);
+			}
+			for (unsigned int p = 0; p < pad_size; ++p)
+				stream.put(0);
 		}
 	}
 }
